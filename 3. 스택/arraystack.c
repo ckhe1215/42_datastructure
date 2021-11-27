8f10a5a272ec638dd1390cc4b8b7d8dbd4e4e6c7
ckhe1215 <ckhe1215@gmail.com>
2021-11-27 22:13:15 +0900

    stack header

---
+++ 3. 스택/arraystack.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arraystack.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkim <hkim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/27 22:05:45 by hkim              #+#    #+#             */
+/*   Updated: 2021/11/27 22:13:06 by hkim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "arraystack.h"
+
+ArrayStack* createArrayStack(int maxElementCount)
+{
+
+}
+
+int pushAS(ArrayStack* pStack, ArrayStackNode element)
+{
+
+}
+
+ArrayStackNode* popAS(ArrayStack* pStack)
+{
+
+}
+
+ArrayStackNode* peekAS(ArrayStack* pStack)
+{
+
+}
+
+void deleteArrayStack(ArrayStack* pStack)
+{
+
+}
+
+int isArrayStackFull(ArrayStack* pStack)
+{
+
+}
+
+int isArrayStackEmpty(ArrayStack* pStack)
+{
+
+}
+
+int main()
+{
+
+}
